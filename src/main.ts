a3fc5af9933b10e166c8c9872cbc83f38ea6f995
ApiCartOps <api.cart.ops@outlook.com>
2024-11-01 21:57:31 +0530

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { ParentComponent } from './app/parent/parent.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [ParentComponent, ReactiveFormsModule, FormsModule],
+  template: `
+    <h1>Hello from {{ name }}!</h1>
+    <a target="_blank" href="https://angular.dev/overview">
+      Learn more about Angular
+    </a>
+    <app-parent />
+  `,
+})
+export class App {
+  name = 'Angular';
+}
+
+bootstrapApplication(App);
